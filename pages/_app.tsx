f18f85b887ae444f55cf58236c9e0d848185de01
Cj-K-7 <kllt7059@gmail.com>
2022-04-24 15:06:28 +0900

    clean up modules for deploy

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,23 +1,12 @@
 import "../styles/globals.scss";
 import type { AppProps } from "next/app";
 import Header from "../components/Header";
-import {
-  Hydrate,
-  QueryClient,
-  QueryClientProvider,
-} from "react-query";
-
-const queryClient = new QueryClient();
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <>
-      <QueryClientProvider client={queryClient}>
-        <Hydrate state={pageProps.dehydratedState}>
-          <Header />
-          <Component {...pageProps} />
-        </Hydrate>
-      </QueryClientProvider>
+      <Header />
+      <Component {...pageProps} />
     </>
   );
 }
